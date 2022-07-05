c26844f846093dda5c9eb162dc9429750e4c6dfb
mac <omigiev1@gmail.com>
2022-07-05 20:45:20 +0100

    Added new folders

---
+++ src/components/common/DashboardHeader/DashbordHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import headerstyle from "./Header.module.css";
+import { IoIosArrowForward } from "react-icons/io";
+
+function Header() {
+  return (
+    <div className={headerstyle.page_header}>
+      <div className={headerstyle.page_header_wrapper}>
+        <p>
+          Dashboard{" "}
+          <span>
+            <IoIosArrowForward className="icon" />
+          </span>{" "}
+          Funding{" "}
+          <span>
+            <IoIosArrowForward className="icon" />
+          </span>{" "}
+          Pay with Nerve
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
