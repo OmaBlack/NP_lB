07b2478b7be8bb9c3210e1234085be629e29c049
mac <omigiev1@gmail.com>
2022-07-07 20:13:14 +0100

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,30 @@ import Signup from "./components/signup/Signup";
 import ProjectFinancing from './components/projectFinancing/ProjectFinancing';
 import RequestSubmitted from './components/request-submitted/RequestSubmitted';
 import CreditHistory from './components/CreditHistory/CreditHistory';
-import DashboardSidebar from './components/Common/DashboardSidebar/DashboardSidebar';
-import CreditformDetails2 from './components/Common/CreditformDetails2/CreditformDetails2';
-import Eclipse from "./components/Common/Eclipse/Eclipse"
+import {Route, Routes} from 'react-router-dom';
 
-const App = () => {
+
+
+
+
+function App() {
+  const location = window.location.pathname;
   return (
-    <>
-    {/* <Signup/> */}
-    {/* <Login /> */}
-    {/* <Creditform /> */}
-    {/* <ProjectFinancing /> */}
-    {/* <RequestSubmitted /> */}
-    <CreditHistory />
-    {/* <DashboardSidebar /> */}
-    {/* <CreditformDetails2 /> */}
-    {/* <Eclipse /> */}
+  
+    <div>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/creditform" element={<Creditform />} />
+            <Route path="/projectfinancing"element={<ProjectFinancing />} />
+            <Route path="/requestsubmitted" element={<RequestSubmitted />} />
+            <Route path="credithistory" element={<CreditHistory />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
 
-    </>
-
-    
+    </div>
   );
+
 }
 
 export default App;
+
